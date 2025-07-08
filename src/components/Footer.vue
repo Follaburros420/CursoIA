0a39d780e1b86b727dc0e261777d4ee593c53cde
PedroArdila419 <pspsonygol@gmail.com>
2025-07-08 16:31:47 -0500

    feat: personalize footer for portfolio

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,7 +10,7 @@ import Separator from "./ui/separator/Separator.vue";
   >
     <div class="p-10 bg-muted/50 dark:bg-card border rounded-2xl">
       <div
-        class="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8"
+        class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-12 gap-y-8"
       >
         <div class="col-span-full xl:col-span-2">
           <a
@@ -21,126 +21,40 @@ import Separator from "./ui/separator/Separator.vue";
               class="bg-gradient-to-tr from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white"
             />
 
-            <h3 class="text-2xl">LexIA</h3>
+            <h3 class="text-2xl font-bold">Pedro Ardila</h3>
           </a>
         </div>
 
-        <div class="flex flex-col gap-2">
-          <h3 class="font-bold text-lg">Contact</h3>
-          <div>
-            <a
-              href="#"
-              class="opacity-60 hover:opacity-100"
-            >
-              Github
-            </a>
-          </div>
-
-          <div>
-            <a
-              href="#"
-              class="opacity-60 hover:opacity-100"
-            >
-              Twitter
-            </a>
-          </div>
-
-          <div>
-            <a
-              href="#"
-              class="opacity-60 hover:opacity-100"
-            >
-              Instagram
-            </a>
-          </div>
-        </div>
-
-        <div class="flex flex-col gap-2">
-          <h3 class="font-bold text-lg">Platforms</h3>
-          <div>
-            <a
-              href="#"
-              class="opacity-60 hover:opacity-100"
-            >
-              iOS
-            </a>
-          </div>
-
-          <div>
-            <a
-              href="#"
-              class="opacity-60 hover:opacity-100"
-            >
-              Android
-            </a>
-          </div>
-
-          <div>
-            <a
-              href="#"
-              class="opacity-60 hover:opacity-100"
-            >
-              Web
-            </a>
-          </div>
-        </div>
-
-        <div class="flex flex-col gap-2">
-          <h3 class="font-bold text-lg">Help</h3>
-          <div>
-            <a
-              href="#"
-              class="opacity-60 hover:opacity-100"
-            >
-              Contact Us
-            </a>
-          </div>
-
-          <div>
-            <a
-              href="#"
-              class="opacity-60 hover:opacity-100"
-            >
-              FAQ
-            </a>
-          </div>
-
-          <div>
-            <a
-              href="#"
-              class="opacity-60 hover:opacity-100"
-            >
-              Feedback
-            </a>
-          </div>
-        </div>
 
         <div class="flex flex-col gap-2">
           <h3 class="font-bold text-lg">Socials</h3>
           <div>
             <a
-              href="#"
+              href="https://www.linkedin.com/in/pedromaa/"
+              target="_blank"
               class="opacity-60 hover:opacity-100"
             >
-              Twitch
+              LinkedIn
             </a>
           </div>
 
           <div>
             <a
-              href="#"
+              href="https://www.youtube.com/@PedroArdila419"
+              target="_blank"
               class="opacity-60 hover:opacity-100"
             >
-              Discord
+              YouTube
             </a>
           </div>
 
           <div>
             <a
-              href="#"
+              href="https://www.instagram.com/pedromanuelardilaarmirola"
+              target="_blank"
               class="opacity-60 hover:opacity-100"
             >
-              Dribbble
+              Instagram
             </a>
           </div>
         </div>
@@ -150,13 +64,7 @@ import Separator from "./ui/separator/Separator.vue";
       <section className="">
         <h3 class="">
           &copy; 2024 Designed and developed by
-          <a
-            target="_blank"
-            href="https://github.com/leoMirandaa"
-            className="text-primary transition-all border-primary hover:border-b-2"
-          >
-            Leo Miranda
-          </a>
+          <span class="text-primary">Pedro Ardila</span>
         </h3>
       </section>
     </div>
