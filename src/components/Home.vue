cf1919f177f2857d12239db63546effd2b376285
PedroArdila419 <pspsonygol@gmail.com>
2025-07-25 16:17:05 -0500

    correcciones 5

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,6 +4,13 @@ import FAQ from './FAQ.vue';
 import Pricing from './Pricing.vue';
 import HowItWorks from './HowItWorks.vue';
 import VideoPreview from './VideoPreview.vue';
+import Benefits from './Benefits.vue';
+import Community from './Community.vue';
+import Contact from './Contact.vue';
+import Testimonials from './Testimonials.vue';
+import Services from './Services.vue';
+import Sponsors from './Sponsors.vue';
+import Team from './Team.vue';
 </script>
 
 <template>
@@ -13,6 +20,13 @@ import VideoPreview from './VideoPreview.vue';
     <Pricing />
     <FAQ />
     <VideoPreview />
+    <Benefits />
+    <Community />
+    <Services />
+    <Testimonials />
+    <Team />
+    <Sponsors />
+    <Contact />
   </div>
 </template>
 
