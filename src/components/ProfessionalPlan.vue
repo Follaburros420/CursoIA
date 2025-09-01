5c0d6658878e49ca3a61540ae8b60b0d409e6b34
PedroArdila419 <pspsonygol@gmail.com>
2025-09-01 18:56:23 -0500

    si

--- src/components/ProfessionalPlan.vue
+++ src/components/ProfessionalPlan.vue
@@ -14,8 +14,8 @@ import Footer from './Footer.vue';
 
 // State management
 const loading = ref(false);
-const originalAmount = ref(9900000); // 99,000 COP in centavos
-const currentAmount = ref(9900000); // 99,000 COP in centavos
+const originalAmount = ref(44900000); // 449,000 COP in centavos
+const currentAmount = ref(44900000); // 449,000 COP in centavos
 
 // Event handlers
 const handleAmountChanged = (newAmount: number) => {
